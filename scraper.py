56424af409396c657f9bafc235ee3f22fc9f11fe
Robert England <46442441+RobertEngland@users.noreply.github.com>
2019-02-19 11:44:42 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,10 +8,10 @@ print("hello")
 # # Read in a page
 html = scraperwiki.scrape("http://foo.com")
 print(html)
-#
+# created a variable called html which grabs the web address and then prints it
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
+root = lxml.html.fromstring(html)
+print(root.cssselect("div[align='left']"))
 #
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
